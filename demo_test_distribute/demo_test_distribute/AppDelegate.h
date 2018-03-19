98c5f71c54e607395533728189674faef0c40910
jsonsnow <1183010734@qq.com>
2018-03-19 17:13:05 +0800

    inital project

---
+++ demo_test_distribute/demo_test_distribute/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  demo_test_distribute
+//
+//  Created by chen liang on 2018/3/19.
+//  Copyright © 2018年 chen liang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ demo_test_distribute/demo_test_distribute/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  demo_test_distribute
+//
+//  Created by chen liang on 2018/3/19.
+//  Copyright © 2018年 chen liang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
